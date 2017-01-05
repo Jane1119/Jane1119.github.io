a3f48466ef753cb4db861b39e0cae25d90b3845b
JaneJoungyeunLee <jane@Janes-MBP.fios-router.home>
2017-01-05 03:01:19 -0500

    Update

---
+++ Table.js
@@ -0,0 +1,252 @@
+var wx = window.screenTop;
+var wy = window.screenLeft;
+
+
+
+var turkey_x = wx + 400;
+var turkey_y = wy + 650;
+
+setTimeout(function(){
+  window.open("turkey.html", "_blank", "scrollbars=no, resizable=no,  top="+turkey_x+", left="+turkey_y+", width=140, height=88")}, 10000);
+
+
+var one_x = wx + 180;
+var one_y = wy + 120;
+
+setTimeout(function(){
+  window.open("1.html", "_blank", "scrollbars=no, resizable=no, top="+one_x+", left="+one_y+", width=460, height=210")}, 30000);
+
+
+var two_x = wx + 260;
+var two_y = wy + 910;
+
+setTimeout(function(){
+  window.open("1-2.html", "_blank", "scrollbars=no, resizable=no, top="+two_x+", left="+two_y+", width=390, height=340")}, 40000);
+
+
+var three_x = wx + 80;
+var three_y = wy + 120;
+
+setTimeout(function(){
+  window.open("1-3.html", "_blank", "scrollbars=no, resizable=no, top="+three_x+", left="+three_y+", width=535, height=300")}, 53000);
+
+
+var four_x = wx + 490;
+var four_y = wy + 70;
+
+setTimeout(function(){
+  window.open("1-4.html", "_blank", "scrollbars=no, resizable=no, top="+four_x+", left="+four_y+", width=520, height=300")}, 66000);
+
+
+var five_x = wx + 150;
+var five_y = wy + 400;
+
+setTimeout(function(){
+  window.open("1-5.html", "_blank", "scrollbars=no, resizable=no, top="+five_x+", left="+five_y+", width=460, height=210")}, 83000);
+
+
+var six_x = wx + 50;
+var six_y = wy + 150;
+
+setTimeout(function(){
+    window.open("1-6.html", "_blank", "scrollbars=no, resizable=no, top="+six_x+", left="+six_y+", width=250, height=300")}, 89000);
+
+
+var seven_x = wx + 100;
+var seven_y = wy + 800;
+
+setTimeout(function(){
+    window.open("1-7.html", "_blank", "scrollbars=no, resizable=no, top="+seven_x+", left="+seven_y+", width=530, height=270")}, 92000);
+
+
+var eight_x = wx + 500;
+var eight_y = wy + 60;
+
+setTimeout(function(){
+    window.open("1-8.html", "_blank", "scrollbars=no, resizable=no, top="+eight_x+", left="+eight_y+", width=400, height=300")}, 106000);
+
+
+var eight2_x = wx + 550;
+var eight2_y = wy + 500;
+
+setTimeout(function(){
+    window.open("1-13.html", "_blank", "scrollbars=no, resizable=no, top="+eight2_x+", left="+eight2_y+", width=300, height=200")}, 115000);
+
+
+var nine_x = wx + 540;
+var nine_y = wy + 790;
+
+setTimeout(function(){
+    window.open("1-9.html", "_blank", "scrollbars=no, resizable=no, top="+nine_x+", left="+nine_y+", width=500, height=250")}, 85000);
+
+
+var ten_x = wx + 150;
+var ten_y = wy + 120;
+
+setTimeout(function(){
+    window.open("1-10.html", "_blank", "scrollbars=no, resizable=no, top="+ten_x+", left="+ten_y+", width=500, height=400")}, 110000);
+
+
+var ele_x = wx + 580;
+var ele_y = wy + 900;
+
+setTimeout(function(){
+    window.open("1-11.html", "_blank", "scrollbars=no, resizable=no, top="+ele_x+", left="+ele_y+", width=400, height=200")}, 120000);
+
+
+var twel_x = wx + 180;
+var twel_y = wy + 80;
+
+setTimeout(function(){
+    window.open("1-12.html", "_blank", "scrollbars=no, resizable=no, top="+twel_x+", left="+twel_y+", width=490, height=290")}, 134000);
+
+
+var F1_x = wx + 610;
+var F1_y = wy + 80;
+
+setTimeout(function(){
+    window.open("2-1.html", "_blank", "scrollbars=no, resizable=no, top="+F1_x+", left="+F1_y+", width=480, height=200")}, 145000);
+
+
+var F2_x = wx + 50;
+var F2_y = wy + 1000;
+
+setTimeout(function(){
+    window.open("2-2.html", "_blank", "scrollbars=no, resizable=no, top="+F2_x+", left="+F2_y+", width=400, height=300")}, 147000);
+
+
+var F3_x = wx + 300;
+var F3_y = wy + 480;
+
+setTimeout(function(){
+    window.open("2-3.html", "_blank", "scrollbars=no, resizable=no, top="+F3_x+", left="+F3_y+", width=500, height=250")}, 142000);
+
+
+var F4_x = wx + 400;
+var F4_y = wy + 700;
+
+setTimeout(function(){
+    window.open("2-4.html", "_blank", "scrollbars=no, resizable=no, top="+F4_x+", left="+F4_y+", width=600, height=340")}, 146000);
+
+
+var F5_x = wx + 130;
+var F5_y = wy + 100;
+
+setTimeout(function(){
+    window.open("2-5.html", "_blank", "scrollbars=no, resizable=no, top="+F5_x+", left="+F5_y+", width=700, height=390")}, 148000);
+
+
+var F6_x = wx + 450;
+var F6_y = wy + 800;
+
+setTimeout(function(){
+    window.open("2-6.html", "_blank", "scrollbars=no, resizable=no, top="+F6_x+", left="+F6_y+", width=550, height=350")}, 152000);
+
+
+var F7_x = wx + 100;
+var F7_y = wy + 100;
+
+setTimeout(function(){
+    window.open("2-7.html", "_blank", "scrollbars=no, resizable=no, top="+F7_x+", left="+F7_y+", width=530, height=260")}, 154000);
+
+
+var F8_x = wx + 300;
+var F8_y = wy + 750;
+
+setTimeout(function(){
+    window.open("2-8.html", "_blank", "scrollbars=no, resizable=no, top="+F8_x+", left="+F8_y+", width=440, height=480")}, 155000);
+
+
+var F9_x = wx + 400;
+var F9_y = wy + 150;
+
+setTimeout(function(){
+    window.open("2-9.html", "_blank", "scrollbars=no, resizable=no, top="+F9_x+", left="+F9_y+", width=420, height=400")}, 156000);
+
+
+var L1_x = wx + 390;
+var L1_y = wy + 100;
+
+setTimeout(function(){
+    window.open("3-1.html", "_blank", "scrollbars=no, resizable=no, top="+L1_x+", left="+L1_y+", width=700, height=400")}, 158000);
+
+
+var L2_x = wx + 150;
+var L2_y = wy + 820;
+
+setTimeout(function(){
+    window.open("3-2.html", "_blank", "scrollbars=no, resizable=no, top="+L2_x+", left="+L2_y+", width=550, height=200")}, 163000);
+
+
+var L3_x = wx + 300;
+var L3_y = wy + 130;
+
+setTimeout(function(){
+    window.open("3-3.html", "_blank", "scrollbars=no, resizable=no, top="+L3_x+", left="+L3_y+", width=300, height=300")}, 168000);
+
+
+var L4_x = wx + 490;
+var L4_y = wy + 850;
+
+setTimeout(function(){
+    window.open("3-4.html", "_blank", "scrollbars=no, resizable=no, top="+L4_x+", left="+L4_y+", width=470, height=300")}, 170000);
+
+
+var L5_x = wx + 80;
+var L5_y = wy + 20;
+
+setTimeout(function(){
+    window.open("3-5.html", "_blank", "scrollbars=no, resizable=no, top="+L5_x+", left="+L5_y+", width=600, height=250")}, 173000);
+
+
+var L6_x = wx + 540;
+var L6_y = wy + 690;
+
+setTimeout(function(){
+    window.open("3-6.html", "_blank", "scrollbars=no, resizable=no, top="+L6_x+", left="+L6_y+", width=640, height=280")}, 175000);
+
+
+var L7_x = wx + 50;
+var L7_y = wy + 360;
+
+setTimeout(function(){
+    window.open("3-7.html", "_blank", "scrollbars=no, resizable=no, top="+L7_x+", left="+L7_y+", width=400, height=250")}, 179000);
+
+
+var L8_x = wx + 140;
+var L8_y = wy + 610;
+
+setTimeout(function(){
+    window.open("3-8.html", "_blank", "scrollbars=no, resizable=no, top="+L8_x+", left="+L8_y+", width=250, height=200")}, 184000);
+
+
+var L9_x = wx + 490;
+var L9_y = wy + 80;
+
+setTimeout(function(){
+    window.open("3-9.html", "_blank", "scrollbars=no, resizable=no, top="+L9_x+", left="+L9_y+", width=380, height=300")}, 183000);
+
+
+var L10_x = wx + 420;
+var L10_y = wy + 670;
+
+setTimeout(function(){
+    window.open("3-10.html", "_blank", "scrollbars=no, resizable=no, top="+L10_x+", left="+L10_y+", width=760, height=400")}, 190000);
+
+
+var L11_x = wx + 100;
+var L11_y = wy + 140;
+
+    setTimeout(function(){
+        window.open("3-11.html", "_blank", "scrollbars=no, resizable=no, top="+L11_x+", left="+L11_y+", width=480, height=260")}, 187000);
+
+
+var L12_x = wx + 430;
+var L12_y = wy + 40;
+
+setTimeout(function(){
+  window.open("3-12.html", "_blank", "scrollbars=no, resizable=no, top="+L12_x+", left="+L12_y+", width=440, height=380")}, 198000);
+
+
+setTimeout(function(){
+  location.reload();}, 337000);
